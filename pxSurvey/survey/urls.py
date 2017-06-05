1e0202ec3fdea52fcca28d95ed099031d36b9e72
Manish Sharma <mks2064@gmail.com>
2017-06-05 10:06:52 +0000

    urls.py edited online with Bitbucket

--- pxSurvey/survey/urls.py
+++ pxSurvey/survey/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     #url(r'^$', DashboardView.as_view(), name='dashboard'),
-    url(r'^dashboard/$', DashboardView.as_view(), name='dashboard_2'),
+    url(r'^dashboard/$', DashboardView.as_view(), name='dashboard'),
 
     url(r'^accounts/login/$', AdminLogInView.as_view(), name='adminLogIn'),
     url(r'^accounts/logout/$', AdminLogOutView.as_view(), name='adminLogOut'),
