37e08440adc852cddf34126fe4cf507b6bffb050
Manish Sharma <mks2064@gmail.com>
2017-06-05 09:59:54 +0000

    urls.py edited online with Bitbucket

--- pxSurvey/survey/urls.py
+++ pxSurvey/survey/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-    url(r'^$', DashboardView.as_view(), name='dashboard'),
+    #url(r'^$', DashboardView.as_view(), name='dashboard'),
     url(r'^dashboard/$', DashboardView.as_view(), name='dashboard_2'),
 
     url(r'^accounts/login/$', AdminLogInView.as_view(), name='adminLogIn'),
